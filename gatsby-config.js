9e4dae8a7e7530d8d4ade32576d9605d1a774a21
mapso-deploy <kevin@mapso.co>
2021-06-16 18:21:33 -0400

    Added explore page iFrame

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,31 @@
+// module.exports = {
+//   siteMetadata: {
+//     title: "Mapso-Codegen-v2",
+//   },
+//   plugins: ["gatsby-plugin-sitemap"],
+//   {
+//   resolve: "@plasmicapp/loader/gatsby",
+//   options: {
+//     projects: ["4rRjJBDqbJu5P9BrfPUQ2a"],
+//     substitutions: {
+//     components:  [{ name: 'iFrame', path: '.cache/.plasmic/components/IFrame.jsx' },{ name: 'Header', path: '.cache/.plasmic/components/Header.jsx' }],
+//   }
+// } // An array of project ids.
+//   },
+// ],
+// };
+
+
 module.exports = {
   siteMetadata: {
-    title: "Mapso-Codegen-v2",
-  },
-  plugins: ["gatsby-plugin-sitemap",
-  {
-  resolve: "@plasmicapp/loader/gatsby",
-  options: {
-    projects: ["4rRjJBDqbJu5P9BrfPUQ2a"],
-    substitutions: {
-    components: [{ name: 'iFrame', path: 'src/components/iFrame.tsx' },{ name: 'Header', path: 'src/components/Header.tsx' }],
-  }
-} // An array of project ids.
-  },
-],
+      title: "Mapso-Plasmic",
+    },
+  plugins: [
+    {
+      resolve: "@plasmicapp/loader/gatsby",
+      options: {
+        projects: ["4rRjJBDqbJu5P9BrfPUQ2a"],
+      },
+    },
+  ],
 };
--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 import PlasmicLoader from '@plasmicapp/loader';
 
-
-function PageComponent() {
-  // Use hooks to fetch data, read from React context, etc.
-
-  return (
-    <PlasmicLoader projectId="4rRjJBDqbJu5P9BrfPUQ2a"
-    component="Explore"
-    componentProps={{
-    iFrame: {
-      wrap: (content) => <iframe title="Blog" src="https://cascadr.co/blogs/cozykev" frameborder="0" width="100%" height="100%" allowFullScreen></iframe>
-    }
-  }}
-/>
-  );
+export default function PageComponent() {
+    return <PlasmicLoader projectId="4rRjJBDqbJu5P9BrfPUQ2a"
+      component="Explore"
+      componentProps={{
+        iFrame: {
+          wrap: (content) => <iframe title= "blog" src="https://cascadr.co/blogs/cozykev" width="100%" height="100%" allowFullScreen></iframe>
+        }
+      }}
+    />
 }
