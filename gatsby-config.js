7c1f9dec738615b3ce0e88094de597f5a88afc2d
mapso-deploy <>
2021-06-16 00:06:05 -0400

    Troubleshooting Plasmic loader
    Attempting to get the iframe to work using the plasmic-loader plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "Mapso-Codegen-v2",
   },
-  plugins: ["gatsby-plugin-sitemap"],
+  plugins: ["gatsby-plugin-sitemap",
+  {
+  resolve: "@plasmicapp/loader/gatsby",
+  options: {
+    projects: ["4rRjJBDqbJu5P9BrfPUQ2a"],
+    substitutions: {
+    components: [{ name: 'iFrame', path: 'src/components/iFrame.tsx' },{ name: 'Header', path: 'src/components/Header.tsx' }],
+  }
+} // An array of project ids.
+  },
+],
 };
